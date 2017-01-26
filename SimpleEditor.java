6f3802091b4abdf7caf1fc321493f7f799485ef1
Alejandro Cortes <ale.crts1996@gmail.com>
2017-01-25 19:13:20 -0600

    Added compile funtion to editor and show icons text to know what to do, aditionaly I added the compile function and show tree in the GUI.

--- SimpleEditor.java
+++ SimpleEditor.java
@@ -7,12 +7,17 @@ import javax.swing.text.*;
 import java.awt.*;
 import java.io.*;
 import java.awt.event.*;
-import java.util.Hashtable;
+import java.util.*;
+
+import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.tree.ParseTree;
+import org.antlr.v4.gui.*;
 
 public class SimpleEditor extends JFrame {
 
   private Action openAction = new OpenAction();
   private Action saveAction = new SaveAction();
+  private Action compileAction = new CompileAction();
 
   private JTextComponent textComp;
   private Hashtable actionHash = new Hashtable();
@@ -25,7 +30,7 @@ public class SimpleEditor extends JFrame {
 
   // Create an editor.
   public SimpleEditor() {
-    super("Swing Editor");
+    super("ANTLR IDLE Editor");
     textComp = createTextComponent();
     makeActionsPretty();
 
@@ -67,16 +72,16 @@ public class SimpleEditor extends JFrame {
     JToolBar bar = new JToolBar();
 
     // Add simple actions for opening & saving.
-    bar.add(getOpenAction()).setText("");
-    bar.add(getSaveAction()).setText("");
+    bar.add(getOpenAction()).setText("OPEN");
+    bar.add(getSaveAction()).setText("SAVE");
     bar.addSeparator();
 
     // Add cut/copy/paste buttons.
-    bar.add(textComp.getActionMap().get(DefaultEditorKit.cutAction)).setText("");
+    bar.add(textComp.getActionMap().get(DefaultEditorKit.cutAction)).setText("CUT");
     bar.add(textComp.getActionMap().get(
-              DefaultEditorKit.copyAction)).setText("");
+              DefaultEditorKit.copyAction)).setText("COPY");
     bar.add(textComp.getActionMap().get(
-              DefaultEditorKit.pasteAction)).setText("");
+              DefaultEditorKit.pasteAction)).setText("PASTE");
     return bar;
   }
 
@@ -85,8 +90,10 @@ public class SimpleEditor extends JFrame {
     JMenuBar menubar = new JMenuBar();
     JMenu file = new JMenu("File");
     JMenu edit = new JMenu("Edit");
+    JMenu run = new JMenu("Run");
     menubar.add(file);
     menubar.add(edit);
+    menubar.add(run);
 
     file.add(getOpenAction());
     file.add(getSaveAction());
@@ -95,8 +102,11 @@ public class SimpleEditor extends JFrame {
     edit.add(textComp.getActionMap().get(DefaultEditorKit.copyAction));
     edit.add(textComp.getActionMap().get(DefaultEditorKit.pasteAction));
     edit.add(textComp.getActionMap().get(DefaultEditorKit.selectAllAction));
+    run.add(getCompileAction());
     return menubar;
   }
+  // Subclass can override to use a different open action.
+  protected Action getCompileAction() { return compileAction; }
 
   // Subclass can override to use a different open action.
   protected Action getOpenAction() { return openAction; }
@@ -116,8 +126,8 @@ public class SimpleEditor extends JFrame {
 
   // An action that opens an existing file
   class OpenAction extends AbstractAction {
-    public OpenAction() { 
-      super("Open", new ImageIcon("icons/open.gif")); 
+    public OpenAction() {
+      super("Open", new ImageIcon("icons/open.gif"));
     }
 
     // Query user for a filename and attempt to open and read the file into the
@@ -183,6 +193,42 @@ public class SimpleEditor extends JFrame {
           } catch (IOException x) {}
         }
       }
+    }
+  }
+
+  // An action that compiles the document code.
+  class CompileAction extends AbstractAction {
+    public CompileAction() {
+      super("Compile", new ImageIcon("icons/compile.gif"));
+    }
+
+    // This action will call Antlr to get the Lexer and Parser.
+    public void actionPerformed(ActionEvent ev) {
+
+      System.out.println("Estoy compilando!");
+
+      // create a CharStream that reads from standard input
+      ANTLRInputStream input = new ANTLRInputStream(textComp.getText());
+      // create a lexer that feeds off of input CharStream
+      ExprLexer lexer = new ExprLexer(input);
+      // create a buffer of tokens pulled from the lexer
+      CommonTokenStream tokens = new CommonTokenStream(lexer);
+      // create a parser that feeds off the tokens buffer
+      ExprParser parser = new ExprParser(tokens);
+      ParseTree tree = parser.prog(); // begin parsing at init rule
+      System.out.println(tree.toStringTree(parser)); // print LISP-style tree
+
+      JFrame frame = new JFrame("Antlr AST");
+      JPanel panel = new JPanel();
+      TreeViewer viewr = new TreeViewer(Arrays.asList(
+        parser.getRuleNames()),tree);
+      System.out.println(parser.getRuleNames());
+      viewr.setScale(1.5);//scale a little
+      panel.add(viewr);
+      frame.add(panel);
+      frame.setSize(500, 500);
+      frame.setVisible(true);
+
     }
   }
 }
